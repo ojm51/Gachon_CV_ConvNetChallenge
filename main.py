70004b5e61818939f3fe67ee06bbc94f93e1ba59
NaMinsu <34850422+NaMinsu@users.noreply.github.com>
2022-05-21 00:33:40 +0900

    split model and main code

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 from dataloader import dataset
 from dataloader import transform as tf
 import torchvision.models as models
+from model import ColorizationModel, AverageMeter
 import cv2
 
 
@@ -23,56 +24,6 @@ def tensor2im(input_image, imtype=np.uint8):
         image_numpy = np.tile(image_numpy, (3, 1, 1))
     image_numpy = np.clip((np.transpose(image_numpy, (1, 2, 0))), 0, 1) * 255.0
     return image_numpy.astype(imtype)
-
-
-class ColorizationModel(nn.Module):
-    def __int__(self, mid_input_size=128, global_input_size=512):
-        super(ColorizationModel, self).__init__()
-
-        # encoder
-        resnet = models.resnet18()
-        resnet.conv1.weight = nn.Parameter(resnet.conv1.weight.sum(dim=1).unsqueeze(1).data)
-        self.mid_level_resnet = nn.Sequential(*list(resnet.children())[0:6])
-
-        # decoder
-        self.deconv1_new = nn.ConvTranspose2d(mid_input_size, 128, kernel_size=4, stride=2, padding=1)
-        self.conv1 = nn.Conv2d(mid_input_size, 128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.conv5 = nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1)
-        self.upsample = nn.Upsample(scale_factor=2)
-
-    def forward(self, x):
-        x = self.mid_level_resnet(x)
-
-        x = nn.ReLU(self.bn2(self.conv1(x)))
-        x = self.upsample(x)
-        x = nn.ReLU(self.bn3(self.conv2(x)))
-        x = nn.ReLU(self.conv3(x))
-        x = self.upsample(x)
-        x = nn.ReLU(self.conv4(x))
-        x = self.upsample(self.conv5(x))
-
-        return x
-
-
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val, self.avg, self.sum, self.count = 0, 0, 0, 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
 
 
 def train(loader_train, model_train, crit, opt, epoch):
---
+++ model.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class ColorizationModel(nn.Module):
+    def __int__(self, mid_input_size=128, global_input_size=512):
+        super(ColorizationModel, self).__init__()
+
+        # encoder
+        resnet = models.resnet18()
+        resnet.conv1.weight = nn.Parameter(resnet.conv1.weight.sum(dim=1).unsqueeze(1).data)
+        self.mid_level_resnet = nn.Sequential(*list(resnet.children())[0:6])
+
+        # decoder
+        self.deconv1_new = nn.ConvTranspose2d(mid_input_size, 128, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(mid_input_size, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.conv5 = nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1)
+        self.upsample = nn.Upsample(scale_factor=2)
+
+    def forward(self, x):
+        x = self.mid_level_resnet(x)
+
+        x = nn.ReLU(self.bn2(self.conv1(x)))
+        x = self.upsample(x)
+        x = nn.ReLU(self.bn3(self.conv2(x)))
+        x = nn.ReLU(self.conv3(x))
+        x = self.upsample(x)
+        x = nn.ReLU(self.conv4(x))
+        x = self.upsample(self.conv5(x))
+
+        return x
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val, self.avg, self.sum, self.count = 0, 0, 0, 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
